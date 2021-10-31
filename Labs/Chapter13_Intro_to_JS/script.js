4c5ff909f35233f40118bda9063dc92181b8fd79
Erik <erikzndr@gmail.com>
2021-10-30 22:37:15 -0500

    Lab completed

---
+++ Labs/Chapter13_Intro_to_JS/script.js
@@ -0,0 +1,2 @@
+//document.write("<footer class='bg-dark text-light'>Thank you!</footer>");
+document.getElementById("theFooter").innerHTML = "<footer class='bg-dark text-light'>Thank you!</footer>";
